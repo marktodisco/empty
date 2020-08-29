1688a9d72ea8119af52a74b01516b3b4cecde683
Mark Todisco <marktodisco@gmail.com>
2020-08-28 21:54:12 -0400

    Added logging, tqdm progress bar, and dry_run function.

--- empty/empty.py
+++ empty/empty.py
@@ -1,13 +1,25 @@
 import os
 import argparse
 import sys
+import logging
+from functools import partial
+
+from tqdm import trange
+
+trange = partial(trange, ncols=80)
+logging.basicConfig(
+    filename='debug.log',
+    style='{',
+    level=logging.DEBUG,
+    format='{asctime} - empty - {funcName} - {levelname} - {message}',
+    datefmt='%Y-%m-%d %I:%M:%S %p')
 
 
 def try_func(func, *args, **kwargs):
     try:
         func(*args, **kwargs)
     except Exception as e:
-        print(e)
+        logging.error(e)
 
 
 def empty_dirs(a_dir, remove=False):
@@ -21,21 +33,52 @@ def empty_dirs(a_dir, remove=False):
     return empty
 
 
+def dry_run(args):
+    mapping = {}
+    mapping.update(vars(args))
+    path = os.path.abspath(args.path)
+    msg = (
+        '\nPositional Arguments\n'
+        '--------------------\n'
+        '  path: {path}\n'
+        '\n'
+        'Optional Arguments\n'
+        '------------------\n'
+        '  --max-passes: {max_passes}\n'
+        '\n'
+    )
+    print(msg.format(**mapping))
+    logging.info(f'Dry-run excuted for {path}.')
+
+
 def main():
     parser = argparse.ArgumentParser(
         prog='empty',
         description='Find and delete empty dirs.')
-    parser.add_argument('dir', help='Directory to clean.')
-    parser.add_argument('-m', '--max-passes', type=int, default=5,
-                        choices=range(1, 6),
-                        help='Number of passes to make.')
+    parser.add_argument('path', help='Directory to clean.')
+    parser.add_argument('-m', '--max-passes', type=int, default=1,
+                        help='Number of passes to make. Default is 1.')
+    parser.add_argument('-d', '--dry-run', action='store_true',
+                        help='Print arguments for testing.')
     args = parser.parse_args()
-
-    if not os.path.exists(args.dir):
-        raise FileNotFoundError(args.dir)
     
-    for _ in range(args.max_passes):
-        _ = empty_dirs(args.dir, remove=True)
+    if args.dry_run:
+        dry_run(args)
+        return
+    
+    path = os.path.abspath(args.path)
+    if not os.path.exists(path):
+        logging.error(f'Directory not found: {path}')
+        logging.info('Quitting program.')
+        raise FileNotFoundError(path)
+    
+    logging.info(f'Starting analysis on {path}')
+    empty_counter = 0
+    for _ in trange(args.max_passes):
+        empty = empty_dirs(path, remove=True)
+        empty_counter += len(empty)
+    logging.info(f'Removed {empty_counter} empty folders.')
+    logging.info(f'Analysis of {path} ended cleanly.')
 
 
 if __name__ == '__main__':
